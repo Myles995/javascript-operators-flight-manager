3941edf15e7c8aeae7b59aa770bdc18bba9018bb
Myles McDermott <cmt_myles@hotmail.com>
2022-12-03 12:58:15 +1000

    module2

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,10 @@
 function Flights() {
-
+    calculateNumberOfFlights(numberOfPassengers, flightCapacity) {
+        if numberOfPassengers % flightCapacity == 0;
+        return Flights;
+        } else {
+            return (Flights = flights++);
+        }
+     
 }
 
